85f0b4f59c62bd8c82562c20923c89c6a32dee9e
Jason C <jason.cipriani@gmail.com>
2021-12-09 15:57:20 -0500

    probably works on all pages now
    - fix: now works correctly regardless of how player page was reached (#1) by trying to add button on dom changes rather than once per page load
    - other minor tweaks

--- F1TVBigPlayer.user.js
+++ F1TVBigPlayer.user.js
@@ -1,10 +1,10 @@
 // ==UserScript==
 // @name         F1TV Big Player
 // @namespace    http://tampermonkey.net/
-// @version      1.0
+// @version      1.1.0-rc.1
 // @description  Make F1TV Player Big
 // @author       Jason C
-// @include      /^https?://f1tv.formula1.com/.*[?&]action=play.*/
+// @include      /^https?://f1tv.formula1.com/.*/
 // @icon         https://www.google.com/s2/favicons?domain=formula1.com
 // @grant        none
 // @updateURL    https://github.com/JC3/F1TVBigPlayer/raw/main/F1TVBigPlayer.user.js
@@ -19,34 +19,36 @@
 (function() {
     'use strict';
 
-    var ButtonSVG = "<svg xmlns='http://www.w3.org/2000/svg' width='24' height='24' viewBox='0 0 24 24'><rect x='3' y='5' width='18' height='14' stroke-width='2' stroke='#d0d0d2' fill='none'/></svg>";
+    let ButtonSVG = "<svg xmlns='http://www.w3.org/2000/svg' width='24' height='24' viewBox='0 0 24 24'><rect x='3' y='5' width='18' height='14' stroke-width='2' stroke='#d0d0d2' fill='none'/></svg>";
+    let adding = false;
+    let observer = null;
 
     function MakeFullWidth () {
-        var player = document.querySelector('.inset-video-item-image-container');
+        let player = document.querySelector('.inset-video-item-image-container');
         player.style.setProperty("position", "fixed", "important");
         player.style.top = "0";
         player.style.left = "0";
         player.style.bottom = "0";
         player.style.right = "0";
         player.style.zIndex = 1000;
-        var cntnr = document.body;
+        let cntnr = document.body;
         cntnr.style.overflowY = "hidden";
         document.getElementById("fbpbutton").onclick = MakeDefaultWidth;
     }
 
     function MakeDefaultWidth () {
-        var player = document.querySelector(".inset-video-item-image-container");
+        let player = document.querySelector(".inset-video-item-image-container");
         player.removeAttribute("style");
-        var cntnr = document.body;
+        let cntnr = document.body;
         cntnr.style.overflowY = "auto";
         document.getElementById("fbpbutton").onclick = MakeFullWidth;
     }
 
     function AddPlayerButton () {
-        if (!document.getElementById("fbpbutton")) {
-            var sibling = document.querySelector(".bmpui-ui-fullscreentogglebutton");
+        if (!document.querySelector("#fbpbutton")) {
+            let sibling = document.querySelector(".bmpui-ui-fullscreentogglebutton");
             if (!sibling) { return false; }
-            var button = document.createElement("button");
+            let button = document.createElement("button");
             button.id = "fbpbutton";
             button.type = "button";
             button.setAttribute("class", "bmpui-ui-button bmpui-off");
@@ -57,15 +59,46 @@
             button.innerHTML = '<span class="bmpui-label">Maximize</span>';
             button.setAttribute("style", `background-image:url("data:image/svg+xml,${encodeURIComponent(ButtonSVG)}")`);
             button.onclick = MakeFullWidth;
-            sibling.parentNode.appendChild(button);
+            try {
+                adding = true;
+                sibling.parentNode.appendChild(button);
+            } finally {
+                adding = false;
+            }
+            console.log("F1TVBigPlayer: Added button to player.");
         }
         return true;
     }
 
-    window.MakeFullWidth = MakeFullWidth;
-    window.MakeDefaultWidth = MakeDefaultWidth;
-    window.AddPlayerButton = AddPlayerButton;
+    // button must be added/removed on dom changes rather than on load because
+    // for some navigation paths (like search -> play) the f1tv site rebuilds
+    // the div#app rather than loading a new page. this is also why this script
+    // needs to @include the whole site: we never know where we've started.
+    function InitPlayerMonitor () {
+        if (!observer) {
+            let app = document.querySelector("div#app");
+            if (!app) { return false; }
+            (observer = new MutationObserver(function(muts) {
+                if (!adding) {
+                    for (let mut of muts) {
+                        if (mut.addedNodes.length > 0) {
+                            AddPlayerButton();
+                            break;
+                        }
+                    }
+                }
+            })).observe(app, { subtree: true, childList: true });
+        }
+        return true;
+    }
 
-    let initInt = setInterval(function() { AddPlayerButton() && clearInterval(initInt); }, 500);
+    window.FBP = {
+        MakeFullWidth: MakeFullWidth,
+        MakeDefaultWidth: MakeDefaultWidth,
+        AddPlayerButton: AddPlayerButton,
+        InitPlayerMonitor: InitPlayerMonitor
+    }
+
+    let initInt = setInterval(function() { InitPlayerMonitor() && clearInterval(initInt); }, 500);
 
 })();
